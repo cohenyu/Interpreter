5ad16f28312ddfca84431507ba7ce9b740d22577
Miriel <mirieljerbi@gmail.com>
2018-12-19 01:49:23 +0200

    example2miri

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <ostream>
 
 int main(int argc, char **argv) {
+    cout << "hi";
    //100
     //ShuntingYard* x = new ShuntingYard;
     //Expression *i = x->fromInfixToExp("100 * ( 2 + 12 ) / 14");

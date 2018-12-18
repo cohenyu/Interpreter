cc200eb8d87bc153b41983b18b372707abe675de
yuval <coheen1@gmail.com>
2018-12-18 02:53:08 +0200

    parser

--- Parser.cpp
+++ Parser.cpp
@@ -5,11 +5,17 @@
 #include "Parser.h"
 
 void Parser::parser(vector<string> data) {
-    int i = 0;
-    while (i < data.size()){
-       commands.at(data[i])->calculate();
-
-
+    int index = 0;
+    while (index < data.size()){
+        Command* c1 = commands.at(data[index]);
+        if (c1 != nullptr){
+            index += c1->doCommand(data, index);
+        } else {
+            Command* c2  = commands.at(data[index+1]);
+            if (c2 != nullptr){
+                index += c2->doCommand(data, index);
+            }
+        }
 
     }
 

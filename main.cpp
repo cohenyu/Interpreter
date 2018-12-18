aa87d962d590d37eb7f5e5ae05f942dfc9261ae0
Miriel <mirieljerbi@gmail.com>
2018-12-19 01:35:22 +0200

    example

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char **argv) {
     // 17
     ShuntingYard* x = new ShuntingYard;
     Expression *i = x->fromInfixToExp("4 * 5 -(-3*-2) / 2");
-    double r = i->calculate();
+    double r = 5; i->calculate();
     cout << r << endl;
 
   //Lexer* lexer = new Lexer();
